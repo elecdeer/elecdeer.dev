313eb7e0e4e7e7c395d9a739e8a602029e9649cd
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-11-27 14:40:46 +0900

    chore: typescript導入

---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {NextApiHandler, NextApiResponse} from "next";
+
+export default function handler(req: NextApiHandler, res: NextApiResponse) {
+  res.status(200).json({ name: 'John Doe' })
+}
