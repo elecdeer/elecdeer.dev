0b12903e0a2d567407dfcbbd7087f52efa50f2ae
elecdeer <37701077+elecdeer@users.noreply.github.com>
2021-11-27 14:43:20 +0900

    chore: prettier導入

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import {NextApiHandler, NextApiResponse} from "next";
+import { NextApiHandler, NextApiResponse } from "next";
 
 export default function handler(req: NextApiHandler, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
