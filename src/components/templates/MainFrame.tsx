88106ffa937738ce1f51bc6bf98ba0612f0c1e11
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-02-21 17:26:11 +0900

    feat: titleを追加

--- src/components/templates/MainFrame.tsx
+++ src/components/templates/MainFrame.tsx
@@ -12,6 +12,7 @@ import {
 import { motion, Transition } from "framer-motion";
 import { RouteDepthChangeContext } from "../../pages/_app";
 import { PageBackLink } from "../molecules/PageBackLink";
+import Head from "next/head";
 
 type Props = {
   title: string;
@@ -54,58 +55,67 @@ export const MainFrame: React.FC<Props> = ({ title, backLink, children }) => {
   const depthChange = useContext(RouteDepthChangeContext);
 
   return (
-    <HStack h={"100vh"} w={"100vw"} position={"absolute"} spacing={"0px"}>
-      <Flex h={"full"} w={"50%"}>
-        <MotionBox
-          p={4}
-          position={"absolute"}
-          h={"100%"}
-          w={"50%"}
-          transformOrigin={"right"}
-          translateX={"0%"}
-          zIndex={10}
-          style={{
-            WebkitBackfaceVisibility: "hidden",
-          }}
-          backgroundColor={"green.300"}
-          initial={depthChange === "DEEP" ? { rotateY: -180 } : { rotateY: 0 }}
-          animate={{ rotateY: 0 }}
-          exit={
-            depthChange === "DEEP"
-              ? { rotateY: 0 }
-              : { rotateY: -180, zIndex: 100 }
-          }
-          transition={transition}
-        >
-          {leftContent}
-        </MotionBox>
-      </Flex>
-      <Flex h={"full"} w={"50%"}>
-        <MotionBox
-          p={4}
-          position={"absolute"}
-          h={"100%"}
-          w={"50%"}
-          transformOrigin={"left"}
-          translateX={"0%"}
-          zIndex={10}
-          style={{
-            WebkitBackfaceVisibility: "hidden",
-          }}
-          backgroundColor={"white"}
-          initial={depthChange === "DEEP" ? { rotateY: 0 } : { rotateY: -180 }}
-          animate={{ rotateY: 0 }}
-          exit={
-            depthChange === "DEEP"
-              ? { rotateY: -180, zIndex: 100 }
-              : { rotateY: 0 }
-          }
-          transition={transition}
-          overflowY={"scroll"}
-        >
-          {rightContent}
-        </MotionBox>
-      </Flex>
-    </HStack>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <HStack h={"100vh"} w={"100vw"} position={"absolute"} spacing={"0px"}>
+        <Flex h={"full"} w={"50%"}>
+          <MotionBox
+            p={4}
+            position={"absolute"}
+            h={"100%"}
+            w={"50%"}
+            transformOrigin={"right"}
+            translateX={"0%"}
+            zIndex={10}
+            style={{
+              WebkitBackfaceVisibility: "hidden",
+            }}
+            backgroundColor={"green.300"}
+            initial={
+              depthChange === "DEEP" ? { rotateY: -180 } : { rotateY: 0 }
+            }
+            animate={{ rotateY: 0 }}
+            exit={
+              depthChange === "DEEP"
+                ? { rotateY: 0 }
+                : { rotateY: -180, zIndex: 100 }
+            }
+            transition={transition}
+          >
+            {leftContent}
+          </MotionBox>
+        </Flex>
+        <Flex h={"full"} w={"50%"}>
+          <MotionBox
+            p={4}
+            position={"absolute"}
+            h={"100%"}
+            w={"50%"}
+            transformOrigin={"left"}
+            translateX={"0%"}
+            zIndex={10}
+            style={{
+              WebkitBackfaceVisibility: "hidden",
+            }}
+            backgroundColor={"white"}
+            initial={
+              depthChange === "DEEP" ? { rotateY: 0 } : { rotateY: -180 }
+            }
+            animate={{ rotateY: 0 }}
+            exit={
+              depthChange === "DEEP"
+                ? { rotateY: -180, zIndex: 100 }
+                : { rotateY: 0 }
+            }
+            transition={transition}
+            overflowY={"scroll"}
+          >
+            {rightContent}
+          </MotionBox>
+        </Flex>
+      </HStack>
+    </>
   );
 };
